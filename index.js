85085c9574d5fa807d963307fd42d0084cec201b
RonaldMartinez00 <mronald16@yahoo.com>
2023-01-01 17:14:24 -0800

    added work into all three files, still needs work.

---
+++ index.js
@@ -0,0 +1,114 @@
+var lat
+var lon
+const form = document.getElementById('myForm');
+var weather // data.weather
+var cityName //data.city.name
+
+
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  var input = form.querySelector('#myinput');
+  const value = input.value;
+
+  localStorage.setItem('city', value);
+
+  function getApi1() {
+    const city = localStorage.getItem('city');
+    console.log(city)
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=a803dcd93207387f5f068d610b14840b`;
+
+    fetch(requestUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        lat = data[0].lat
+        lon = data[0].lon
+         
+        getapi2();
+        
+      });
+      
+  };
+
+ //function mapApi(){
+    
+//weather[1].map((i) => {
+    //var i = 0
+    //function displayApi(Element, className, text){
+        
+        //var newElement = document.createElement(Element)
+        //newElement.className = className
+
+        //newElement.textContent = text
+        //document.getElementById('container2').appendChild(newElement)
+        //i++
+
+        //console.log(newElement)
+        //console.log(weather[1][i])
+    //};
+
+
+ //displayApi('H2', 'city-name', weather[1]);
+
+    
+
+//})
+
+//};
+
+//Chatgpt//
+function mapApi() {
+    const container = document.getElementById('container2');
+    const cityName = weather[0];
+    const weatherData = weather[1];
+  
+    // Create a heading element for the city name
+    const cityHeading = document.createElement('h2');
+    cityHeading.className = 'city-name';
+    cityHeading.textContent = cityName;
+    container.appendChild(cityHeading);
+  
+    // Map the weather data array and create elements for each day
+    weatherData.map((day) => {
+      const dayDiv = document.createElement('div');
+      dayDiv.className = 'day';
+  
+      const date = document.createElement('p');
+      date.className = 'date';
+      date.textContent = day.date;
+      dayDiv.appendChild(date);
+  
+      const temp = document.createElement('p');
+      temp.className = 'temp';
+      temp.textContent = `Temperature: ${day.temp}`;
+      dayDiv.appendChild(temp);
+  
+      // Add more weather information as needed
+  
+      container.appendChild(dayDiv);
+    });
+  }
+  
+
+//
+
+function getapi2(){
+    var requestUrl2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&appid=a803dcd93207387f5f068d610b14840b&units=imperial`
+
+    fetch(requestUrl2)
+    .then((response) => response.json())
+    .then((data) => {
+   
+    weather = [data.city.name, data.list];
+
+    console.log(weather)
+    mapApi()
+    });
+    };
+    getApi1();
+});
+
+console.log(lat,lon);
+
